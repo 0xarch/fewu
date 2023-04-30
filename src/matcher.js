c3183ec1bcf68216d5c22b52160e1662d3b0ec8c
0xarch <alanqa-ops@outlook.com>
2023-04-30 23:42:34 +0800

    update

--- src/matcher.js
+++ src/matcher.js
@@ -36,6 +36,29 @@ function parseWidget(content) {
     return content;
 }
 
+/*
+    本处代码为 Hogger 的 EJS模板 提供了独特的class简写和奇异CSS支持
+
+    class简写：
+        例如：
+            <div class="foo bar">
+        可以被写成
+            <div.foo.bar>
+        也可以是
+            <div [foo bar]>
+        注意：
+            前者只能书写简略元素(只有class的元素)，后者可以书写普通元素(带有其他属性的元素)
+            例如：
+                <a class="foo" href="/bar">
+            可以使用后者，看上去像
+                <a [foo] href="/bar">
+    奇异CSS:
+        例如:
+            class="flexRow flexAlignCenter" class="flexColumn flexAlignCenter flexJustifyCenter"
+        可以被书写为：
+            class=":fR_fAC" class=":fR_fAJC"
+        你可以通过在 <DEFINE> 标记的地方添加replace来使用自己的奇异CSS
+*/
 function parseCSS(content){
     var content = content.replace(/<([^.^\/^ ^>]*?)\.([^>^"]*?)>/g,'<$1 class="$2">');
     content = content.replace(/<([^ ^<^\/]*?)\s*?\[([\s\S]*?)\]\s*?(.*?)?>/g,'<$1 class="$2" $3>');
@@ -47,8 +70,9 @@ function parseCSS(content){
         classes.push(match[1]);
     }
     for (const key of classes) {
+        // <DEFINE>
         const c = key.replace(':fR_',' flexRow :').replace(':fC_',' flexColumn :').replace(':fI_',' flexItem :')
-                     .replace(':fJC','flexJC').replace(':fAC','flexAC').replace(':fAJC','flexAC flexJC')
+                     .replace(':fJC','flexJustifyCenter').replace(':fAC','flexAlignCenter').replace(':fAJC','flexAlignCenter flexJustifyCenter')
                      .replace(':mw',' marginLeft marginRight').replace(':mh',' marginTop marginBottom')
                      .replace(/\./g," ");
         content = content.replace(`class="${key}"`,`class="${c}"`);
