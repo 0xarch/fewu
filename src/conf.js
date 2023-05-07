9954530bf3762b6649112361cb7f1ef87e43fbf7
0xarch <alanqa-ops@outlook.com>
2023-05-07 18:15:27 +0800

    delete js codes !only develop on Deno/TS

--- src/conf.js
+++
@@ -1,8 +0,0 @@
-const read = require("fs").readFileSync;
-const CONF_DIR = "./conf/conf.json";
-const CONF = JSON.parse(read(CONF_DIR,(err)=>{
-    if(err){
-        LOG("<Failed> Met an error while reading configuration file!");
-    }
-}));
-exports.CONF=CONF;
