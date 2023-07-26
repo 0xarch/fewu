12e0b445da1693892f783f9e419da62c075bed2d
0xarch <alanqa-ops@outlook.com>
2023-07-26 14:06:16 +0800

    theme : let Palette visible in global

--- conf/theme/Default/main.js
+++ conf/theme/Default/main.js
@@ -8,11 +8,12 @@ const Colors = {
     "BROWN": [27, 38.2]
 }
 
+const Palette = new MPaletteProcesser(Colors);
+Palette.setPaletteFrom("primary", "GREEN", 500,true);
+Palette.setPaletteFrom("secondary", "BROWN", 500);
+
 window.onload=()=>{
     MInitSet.ActionTitle();
     MInitSet.Wind();
     hljs.highlightAll();
-    const Palette = new MPaletteProcesser(Colors);
-    Palette.setPaletteFrom("primary", "GREEN", 500,true);
-    Palette.setPaletteFrom("secondary", "BROWN", 500);
 }
