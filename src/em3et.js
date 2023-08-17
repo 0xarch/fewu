7fb9682502a72a5c79586332ec40569425cc5c4c
0xarch <alanqa-ops@outlook.com>
2023-08-17 20:15:46 +0800

    update

--- src/em3et.js
+++ src/em3et.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 /**
  *
  * @param { element } element
@@ -13,9 +16,16 @@ function render(element){
  * @param { string } raw_string
  * @returns { Array }
  */
-function Em3et_AST(raw_string){
-    let stack = new Array;
-    let raw_chars = raw_string.split("");
+function Em3et_AST(raw_string,file_dir){
+    let p_string = raw_string;
+    let matches = raw_string.matchAll(/\*([^*]+?)\*/g);
+    let quo = false;
+    for(let item of matches){
+        p_string = p_string.replace(item[0],Em3et_FindFile(file_dir,item[1]));
+    }
+    console.log(p_string);
+    let stack = [];
+    let raw_chars = p_string.split("");
     let i = 0,len = raw_chars.length;
     while(i<len){
         let _char_now = raw_chars[i];
@@ -34,33 +44,18 @@ function Em3et_AST(raw_string){
                 }
             }
         }else
-        if(_char_now=='('){
-            stack.push('(');
-            let _str = new String;
+        if(['(',':','%','*','"',"'"].includes(_char_now)){
+            if(!['"',"'"].includes(_char_now)) stack.push(_char_now);
+            let _str = "";
+            let _end_char = _char_now!='('?_char_now:')';
             i++;
             while(i<len){
                 let __char_now = raw_chars[i];
-                if(__char_now!=')'){
-                    _str = _str+__char_now;
-                    i++;
-                }else{
-                    stack.push(_str);
-                    break;
-                }
-            }
-        }else
-        if(_char_now==':'){
-            stack.push(':');
-            let _str = new String;
-            i++;
-            while(i<len){
-                let __char_now = raw_chars[i];
-                if(__char_now!=':'){
+                if(__char_now!=_end_char){
                     if(__char_now!='\\'){
-                        _str = _str+__char_now;
+                        _str += __char_now;
                     }else{
-                        i++;
-                        _str = _str+raw_chars[i];
+                        _str += raw_chars[++i];
                     }
                     i++;
                 }else{
@@ -69,54 +64,117 @@ function Em3et_AST(raw_string){
                 }
             }
         }else
+        if(_char_now==','){
+            if(quo){
+                stack.push('}');
+                quo=false;
+            }else{
+                stack.push('{');
+                quo=true;
+            }
+        }else
         if(!/[ \n]/.test(_char_now)) stack.push(_char_now);
         i++;
     }
-    return stack;
+    return stack
 }
 
 /**
  *
  * @param { Array } stack
+ * @param { JSON } argument
  * @returns { String }
  */
-function Em3et_Compile(stack){
-    let result = new String;
+function Em3et_Compile(stack,argument){
+    let result = '';
     let i=0,len = stack.length;
-    let element_stack = new Array;
+    let element_stack = [];
+    result = 'let str = new String; str+=`';
+    let in_command = false, in_variable = false;
     while(i<len){
         let text = stack[i];
         if(/[A-Za-z]+/.test(text)){
-            result = result+'<'+text;
+            result += '<'+text;
             element_stack.push(text);
         }else{
             switch(text){
                 case '{':
-                    result = result+'>';
+                case '[':
+                    result +='>';
                     break;
                 case '}':
-                    result = `${result}</${element_stack.pop()}>`;
+                case ']':
+                    result += `</${element_stack.pop()}>`;
+                    break;
+                case '!':
+                case '/':
+                    element_stack.pop();
+                    result += '/>';
                     break;
                 case ':':
-                    i++;
-                    result = result+stack[i];
+                    result += stack[++i];
                     break;
                 case '(':
-                    i++;
-                    result = result+' '+stack[i];
+                    result += ' '+stack[++i];
                     break;
                 case '#':
-                    i++;
-                    result = `${result} id="${stack[i]}"`;
+                    result += ` id="${stack[++i]}"`;
                     break;
                 case '.':
-                    i++;
-                    result = `${result} class="${stack[i]}"`;
+                    result += ` class="${stack[++i]}"`;
+                    break;
+                case '%':
+                    if(in_command){
+                        result += '`'+stack[++i]+';str+=`';
+                        in_command = false;
+                    }else{
+                        result += '`;'+stack[++i]+'str+=`';
+                        in_command = true;
+                    }
+                    break;
+                case '$':
+                    if(in_variable){
+                        result += '}';
+                        in_variable=false;
+                    }else{
+                        result += '${'+stack[++i];
+                        in_variable=true;
+                    }
                     break;
             }
         }
         i++;
     }
-    return result;
+    result += '`;str';
+    if(argument)
+        with(argument) return eval(result)
+    else return eval(result)
 }
-exports.render=(element)=>render(element);
+
+function Em3et_FileRide(dir,name){
+    for(let item of fs.readdirSync(dir,{recursive:true})){
+        if(item == name){
+            return fs.readFileSync(path.join(dir,item)).toString()
+        }
+    }
+    return null
+}
+
+function Em3et_FindFile(dir,name){
+    let result = null;
+    if(typeof dir == 'string'){
+        result = Em3et_FileRide(dir,name);
+    }else if(typeof dir == 'object'){
+        for(let item of dir){
+            let v = Em3et_FileRide(item,name);
+            if(v!=null){
+                result = v;
+                break;
+            }
+        }
+    }
+    return result
+}
+exports.render=(element,arg)=>render(element,arg);
+exports.renderText=(text,file_dir,arg)=>Em3et_Compile(Em3et_AST(text,file_dir),arg);
+exports.findFile=(dir,name)=>Em3et_FindFile(dir,name);
