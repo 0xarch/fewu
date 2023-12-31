6ab333d15c7cf9fd864615078e1e5ac87a98969d
0xarch <alanqa-ops@outlook.com>
2023-12-31 20:13:44 +0800

    change theme

--- themes/Next/files/meui.js
+++ themes/Next/files/meui.js
@@ -1,4 +1,6 @@
-MEUI.components.drawer.create(document.getElementById('binding-d0'),document.getElementById('binding-d1'));
+try{
+    MEUI.components.drawer.create(document.getElementById('binding-d0'),document.getElementById('binding-d1'));
+} catch(_) {}
 MEUI.browse.darkModeSettings.setMode();
 MEUI.browse.windowSettings.detectShrink();
 window.onresize = function(){
