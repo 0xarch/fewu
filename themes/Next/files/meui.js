5defda880c1b7181bc10fa77d1b7c6928844b94b
0xarch <alanqa-ops@outlook.com>
2024-01-01 11:17:09 +0800

    theme

--- themes/Next/files/meui.js
+++ themes/Next/files/meui.js
@@ -1,8 +1,14 @@
-try{
-    MEUI.components.drawer.create(document.getElementById('binding-d0'),document.getElementById('binding-d1'));
-} catch(_) {}
-MEUI.browse.darkModeSettings.setMode();
-MEUI.browse.windowSettings.detectShrink();
-window.onresize = function(){
-    MEUI.browse.windowSettings.debounced.detectShrink();
-}
+document.addEventListener('DOMContentLoaded',()=>{
+    try{
+        MEUI.components.drawer.create(document.getElementById('binding-d0'),document.getElementById('binding-d1'));
+    } catch(e) {
+    }
+    try{
+        MEUI.compkits.dwt();
+    }catch(e) {}
+    MEUI.browse.darkModeSettings.setMode();
+    MEUI.browse.windowSettings.detectShrink();
+    window.onresize = function(){
+        MEUI.browse.windowSettings.debounced.detectShrink();
+    }
+})
