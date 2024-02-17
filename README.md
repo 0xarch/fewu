1ef8e1915bfdf887093e2743dd82691d697864e4
0xarch <alanqa-ops@outlook.com>
2024-02-17 16:41:28 +0800

    little fix

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
-# Nexo
+# Nexo Generator
 
-静态博客生成器
+`Nexo` is an automatic generator for static blog pages.
 
-如果你想使用 Nexo , 请按照以下步骤：
+## Using Nexo
 
-## Nodejs
-
-安装 Node.js 及 npm
-
-fork 或 clone 本仓库
-
-因为作者懒得再开一个仓库，所以你要先删除作者的文章 删除 posts/*
-
-```$ npm i``` 安装依赖
-
-```$ npm run start``` 生成
+**For now**
--- src/modules/app/builder.js
+++ src/modules/app/builder.js
@@ -1,5 +1,4 @@
 import * as EJS from '../parser/ejs/app.js';
-import * as RJS from '../parser/rjs/main.js';
 import * as PUG from '../parser/pug/app.js';
 import * as Hail from '../lib/hail.js';
 
@@ -39,7 +38,7 @@ const build = async function(type,template,json,path,ThemeConfig){
 async function build_and_write(type,template,options,provide_variables,theme_config,path_write_to){
     switch(type){
         case 'EJS':
-            Hail.writeFile(EJS.parse(template,{options,provide_variables,theme_config}),path_write_to);
+            Hail.writeFile(EJS.parse(template,{...options,...provide_variables,theme_config}),path_write_to);
         case 'PUG':
         case 'JADE':
             Hail.writeFile(PUG.parse(template,options,provide_variables,theme_config),path_write_to);
