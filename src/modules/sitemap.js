40ea3f563b4ac0f7e9ba1524733239079b6dce20
0xarch <alanqa-ops@outlook.com>
2024-02-25 22:59:56 +0800

    changefreq

--- src/modules/sitemap.js
+++ src/modules/sitemap.js
@@ -10,7 +10,7 @@ function generateSitemapXml(root_url,posts){
     let result = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`;
     result +=`<url><loc>${encodeURI(root_url)}</loc><priority>0.9</priority></url>`;
     for(let v of posts){
-        result+=`<url><loc>${encodeURI(root_url+v.websitePath)}</loc><lastmod>${v.datz.toString()}</lastmod><priority>1.0</priority></url>`;
+        result+=`<url><loc>${encodeURI(root_url+v.websitePath)}</loc><lastmod>${v.datz.toString()}</lastmod><priority>1.0</priority><changefreq>daily</changefreq></url>`;
     }
     result += '</urlset>';
     return result;
