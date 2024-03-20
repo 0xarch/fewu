462a94c2057f3c319ed0d44ef9b580ade5451ff3
0xarch <alanqa-ops@outlook.com>
2024-03-20 20:49:05 +0800

    doc

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/**
+ * @name Nexo
+ * @author Hogger Team, 0xarch(soloev) is the main author.
+ * @home_page https://github.com/0xarch/nexo
+ * @demo_page https://0xarch.github.io
+ * @license GPL v3.0 and later
+ */
+
 import App from './app/app.js';
 
 App();
