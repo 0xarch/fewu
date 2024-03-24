c793eb3abef841aaa9bb8f402182ba4a53dddc1f
0xarch <alanqa-ops@outlook.com>
2024-03-24 15:00:07 +0800

    copr

--- src/core/text_process.js
+++ src/core/text_process.js
@@ -1,3 +1,13 @@
+// ***************************************
+// * Nexo Text Process Module.           *
+// *                                     *
+// *   Copyright 2024 0xarch             *
+// *   Please don't change               *
+// *                                     *
+// *   @Pure                             *
+// *   @Simple                           *
+// ***************************************
+
 /**
  * 
  * @param {string} content 
