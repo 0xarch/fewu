abd39fb64e2a0f73060e7792b6c6148a98265236
Soloev <alanqa-ops@outlook.com>
2024-11-25 23:37:18 +0800

    new utility (unused)

---
+++ _src/util/Markdown.mjs
@@ -0,0 +1,58 @@
+import { Marked } from "marked";
+
+class Markdown {
+    #marked;
+    #usedExtensions = [];
+    /**
+     * @type {import("marked").MarkedOptions}
+     */
+    #parseOptions = {
+        mangle: false
+    };
+
+    /**
+     * 
+     * @param {...import("marked").MarkedExtension} extensions 
+     */
+    constructor(...extensions){
+        this.#marked = new Marked(...extensions);
+        this.#usedExtensions.push(...extensions);
+    }
+
+    /**
+     * 
+     * @param {string} src 
+     * @returns {string}
+     */
+    parse(src){
+        return this.#marked.parse(src,this.#parseOptions);
+    }
+
+    /**
+     * 
+     * @param  {...import("marked").MarkedExtension} extensions 
+     */
+    use(...extensions){
+        this.#marked.use(...extensions);
+        this.#usedExtensions.push(...extensions);
+    }
+
+    /**
+     * 
+     * @param {keyof import("marked").MarkedOptions} key 
+     * @param {import("marked").MarkedOptions[key]} value 
+     */
+    setOption(key,value){
+        this.#parseOptions[key] = value;
+    }
+
+    getUsedExtensions(){
+        return this.#usedExtensions;
+    }
+
+    getOptions(){
+        return this.#parseOptions;
+    }
+}
+
+export default Markdown;
