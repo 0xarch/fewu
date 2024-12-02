83cbc999a63d796bcb2501a702b343b68dab2b83
Soloev <alanqa-ops@outlook.com>
2024-12-03 01:17:27 +0800

    修复 database 在无 --config 传入时崩溃

--- _src/database/main.mjs
+++ _src/database/main.mjs
@@ -2,7 +2,7 @@ import Database from "./database.mjs";
 import Argv from "#util/Argv";
 import { readFileSync, existsSync } from "fs";
 
-let configJsonLocation = Argv.main[0] ?? Argv['--config'][0] ?? Argv['-c'][0] ?? `config.json`;
+let configJsonLocation = Argv.main[0] ?? Argv['--config']?.[0] ?? Argv['-c']?.[0] ?? `config.json`;
 
 if(!existsSync(configJsonLocation)){
     console.error(`No such configuration file: ${configJsonLocation}`);
