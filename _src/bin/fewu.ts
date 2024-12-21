734efd2e019c1238c6c80f9b4b4d05688c4fb144
Soloev <alanqa-ops@outlook.com>
2024-12-21 20:35:26 +0800

    move _src/app to _src/bin/entrance

--- _src/bin/fewu.ts
+++ _src/bin/fewu.ts
@@ -5,13 +5,13 @@ import "#util/Argv";
 async function navigation(){
     let executing_task: Function;
     if(process.argv.includes('--new') || process.argv.includes('-n')){
-        executing_task = (await import("../app/new.mjs")).default;
+        executing_task = (await import("./entrance/new.mjs")).default;
     } else if (process.argv.includes('--init')) {
-        executing_task = (await import("../app/init.mjs")).default;
+        executing_task = (await import("./entrance/init.mjs")).default;
     } else if (process.argv.includes('--help') || process.argv.includes('-h')) {
-        executing_task = (await import("../app/help.mjs")).default;
+        executing_task = (await import("./entrance/help.mjs")).default;
     } else {
-        executing_task = (await import("../app/app.mjs")).default;
+        executing_task = (await import("./entrance/app.mjs")).default;
     }
     executing_task();
 }
