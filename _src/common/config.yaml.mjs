ce8a02ef354eb486bca8e55501431cb42065aa51
Soloev <alanqa-ops@outlook.com>
2025-01-10 15:45:31 +0800

    update config yaml

---
+++ _src/common/config.yaml.mjs
@@ -0,0 +1,17 @@
+const value = _ => `title: Fewu
+description: ''
+author: Meteor
+language: zh-CN
+timezone: Asia/Shanghai
+url: https://blog.myweb.site/
+root: /
+source_dir: source
+public_dir: public
+theme: Next
+excluded_files: 
+  - posts/template.md
+`;
+
+export {
+    value
+};
